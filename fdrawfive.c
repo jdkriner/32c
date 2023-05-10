2f7eb00233cd7614620e449be14c85b2384f8578
jdkriner <jackkriner@icloud.com>
2023-05-09 21:53:37 -0700

    finished file

--- fdrawfive.c
+++ fdrawfive.c
@@ -15,7 +15,8 @@ struct card {
 
 struct card** cards;
 int topPos = 0;
-
+int* scoreList;
+int* playernums;
 
 void fatal(char* msg){
 	printf("%s",msg);
@@ -26,15 +27,60 @@ void usage(){
 }
 
 void push(struct card* cardPointer){
+//	printf("pushing %d number\n", topPos);
 	cards[topPos] = cardPointer;
+//	printf("pushed %d num\n", topPos);
 	topPos++;
 }
 
-int pop(){
-	return 1;
+struct card* pop(){
+	if (topPos == 0){
+		char errormsg[] = "Stack is empty";
+		fatal(errormsg);
+		usage();
+		exit(0);
+	}
+	else{
+		topPos--;
+		return cards[topPos];
+	}
 }
 
 void fillStack(int argc, char **argv){
+	int argc2 = argc-2;
+	if (argc2 % 5 !=0 || argc2 > (atoi(argv[1])*5)){
+		char errormsg[] = "Wrong number of cards";
+		fatal(errormsg);
+		usage();
+		exit(0);
+	}
+	for (int x = 2; x != argc; x++){
+		if (atoi(argv[x]) < 1 || atoi(argv[x]) > 52){
+			char errormsg[] = "Cards must be between 1 and 52";
+			fatal(errormsg);
+			usage();
+			exit(0);
+		}
+		else{
+			int j = atoi(argv[x]);
+			struct card* cardPointer = malloc(sizeof(struct card));
+//			if (j % 13 == 0){
+//				cardPointer->value = 1;
+//			}
+//			else{
+//			cardPointer->value = j+1;
+//			}
+			cardPointer->value = (j%13)+1;
+			strcpy(cardPointer->rank, ranks[(j%13)]);
+			if (j == 52){
+				strcpy(cardPointer->suit, suit[3]);
+			}
+			else{
+				strcpy(cardPointer->suit, suit[j/13]);
+				}
+			push(cardPointer);
+		}
+	}
 }
 
 
@@ -42,10 +88,14 @@ void fillStackWithRandomNumbers(int nPlayers){
 	for (int j = 0; j != nPlayers * 5; j++){
 		int x = (rand() % 52) + 1;
 		struct card* cardPointer = malloc(sizeof(struct card));
-	        cardPointer->value = x;
+	        cardPointer->value = (x%13)+1;
         	strcpy(cardPointer->rank, ranks[(x%13)]);
-        	strcpy(cardPointer->suit, suit[x/13]);
-
+		if (x == 52){
+			strcpy(cardPointer->suit, suit[3]);
+		}
+		else{
+        		strcpy(cardPointer->suit, suit[x/13]);
+		}
 		push(cardPointer);
 	}
 }
@@ -56,26 +106,89 @@ void showStack(){
 	for (int x = topPos; x != 0; x--){
 		temp--;
 		printf("%s of %s, %d\n", (cards[temp])->rank, (cards[temp])->suit, (cards[temp])->value);
+		
 	}
 }
 
+void runGame(int nPlayers){
+	scoreList = malloc(nPlayers*sizeof(int));
+	int itercount = 1;
+	while (itercount <= 5){
+		for (int x = 1; x != nPlayers + 1; x++){
+			struct card* carddrew = pop();
+			scoreList[x] += carddrew->value;
+			printf("Player %d draws %s of %s with value of %d as %d of 5 cards\n", x, carddrew->rank, carddrew->suit, carddrew->value, itercount);
+		}
+		itercount++;
+	}
+	printf("\n");
+}
 
-int main (int argc, char* argv[]){
-//	struct card** cards;
+void showScores(int nPlayers){
+	for (int x = 1; x != nPlayers + 1; x++){
+		printf("Player %d scored %d\n", x, scoreList[x]);
+	}
+	printf("\n");
+}
+
+
+void showWinners(int nPlayers){
+	int maxscore = 0;
+	playernums = malloc(nPlayers * sizeof(int));
+	int tiecount = 0;
+	for (int x = 0; x!= nPlayers; x++){
+		if (scoreList[x] > maxscore){
+			maxscore = scoreList[x];
+			tiecount = 0;
+			playernums[0] = x + 1;
+		}
+		else if (scoreList[x] == maxscore){
+			tiecount++;
+			playernums[0 + tiecount] = x + 1;
+		}
+	}
+	if (tiecount == 0){
+		printf("Player %d won with a score of %d", playernums[0], maxscore);
+	}
+	else{
+		printf("Players ");
+		for (int x = 0; x <= tiecount; x++){
+			if (x == tiecount){
+				printf("and %d ", playernums[x]);
+				continue;
+			}
+			printf("%d, ", playernums[x]);
+		}
+		printf("tied with a score of %d", maxscore);
+	}
+//	for (int x = nPlayers; x != -1; x--){
+//:w		free(playernums[x]);
+//	}
+	free(playernums);
+}
+
+
+
+int main (int argc, char *argv[]){
 	int x = atoi(argv[1]);
 	srand(time(NULL));
 	cards = malloc(x * 5 * sizeof(struct student*));
+//	printf("Size of cards: %ld\nSize of struct student*: %ld\n", sizeof(cards), sizeof(struct student*));
 	if (argc == 2){
 		fillStackWithRandomNumbers(x);
 	}
 	else{
 		fillStack(argc, argv);
 	}
-	showStack();
-	printf("done showStack");
-	for (int x = topPos; x!= -1; x--){
+//	showStack();
+	int topPos2 = topPos;
+	runGame(x);
+	showScores(x);
+	showWinners(x);
+	for (int x = topPos2-1; x!= -1; x--){
 		free(cards[x]);
 	}
 	free(cards);
-	return 1;
+	free(scoreList);
+	return 1;    
 }
